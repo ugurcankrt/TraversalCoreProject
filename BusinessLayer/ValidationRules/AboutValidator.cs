3605046f733c917a64642eec939b437ddbfec37e
Uğurcan Kurt <ugurcan4141@outlook.com.tr>
2023-04-29 11:25:39 +0300

    Add project files.

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator() 
+        { 
+            RuleFor(x=>x.Description).NotEmpty().WithMessage("Bu kısım boş geçilemez.");
+
+            RuleFor(x => x.Description).MinimumLength(20).WithMessage("En az 20 karakter girişi yapınız.");
+            
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("En fazla 1500 karakter girişi yapabilirsiniz.");
+
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Bu kısım boş geçilemez.");
+
+        }
+    }
+}
---
+++ TraversalCoreProject/Models/UserRegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Lütfen adınızı giriniz.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen soyadınızı giriniz.")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı giriniz.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen mailin adresinizi giriniz.")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen parolanızı giriniz.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen parolanızı tekrar giriniz.")]
+        [Compare("Password",ErrorMessage ="Parolalar aynı değil.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
